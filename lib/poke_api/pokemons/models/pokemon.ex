593016ff8598417c1583e5d5d8851646fb0b41cb
Edilberto Morais <edilbertocmorais@gmail.com>
2022-06-30 19:14:52 -0300

    criando o schema pokemon

---
+++ lib/poke_api/pokemons/models/pokemon.ex
@@ -0,0 +1,33 @@
+defmodule PokeApi.Pokemons.Models.Pokemon do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  schema "pokemons" do
+    field :name, :string
+    field :img_url, :string
+
+    timestamps()
+  end
+
+  @required_params [:name, :img_url]
+
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+  end
+
+  # exemplo do André (entender)
+  # def changeset(%__MODULE__{} = model, params) do
+  #   model
+  #   |> cast(params, @required_params)
+  #   |> validate_required(@required_params)
+  # end
+end
